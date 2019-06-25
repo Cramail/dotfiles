7f639b993f2c57f90485aab388faa26ea4a29e4e
Shinohara Yuichi <shinohara@kde.cs.tut.ac.jp>
2019-06-25 15:12:05 +0900

    Output process informations

--- KDE/setup3.sh
+++ KDE/setup3.sh
@@ -1,26 +1,33 @@
 # vim
-mkdir -p ~/.vim/colors
-git clone https://github.com/w0ng/vim-hybrid.git ~/Downloads/vim-hybrid
-cp ~/Downloads/vim-hybrid/colors/hybrid.vim ~/.vim/colors
-
-# bashrc
-echo "PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ '" >> ~/.bashrc
-echo "export EDITOR=vim"
-echo "export LANG=ja_JP.UTF-8"
+if [ -e "~/.vim/colors" ] ; then
+  echo "Create a directory ~/.vim/colors"
+  mkdir -p ~/.vim/colors
+fi
+if [ -e "~/.vim/colors/hybrid.vim" ]; then
+  echo "Download hybrid.vim for vim colorscheme"
+  git clone https://github.com/w0ng/vim-hybrid.git ~/vim-hybrid
+  cp ~/vim-hybrid/colors/hybrid.vim ~/.vim/colors
+fi
 
 # pyenv install
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-echo '##-------- pyenv settings --------##' >> ~/.bashrc
-echo 'export PYENV_HOME=$HOME/.pyenv' >> ~/.bashrc
-echo 'export PATH=$PATH:$PYENV_HOME/bin' >> ~/.bashrc
-echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+if [ -e "~/.pyenv" ] ; then
+  echo "Download pyenv for python"
+  git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+  echo "Add environment variable for pyenv"
+  echo '##-------- pyenv settings --------##' >> ~/.bashrc
+  echo 'export PYENV_HOME=$HOME/.pyenv' >> ~/.bashrc
+  echo 'export PATH=$PATH:$PYENV_HOME/bin' >> ~/.bashrc
+  echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+fi
 
 # anaconda install
+echo "Install Anaconda"
 eval "$(~/.pyenv/bin/pyenv init -)"
 ~/.pyenv/bin/pyenv install anaconda3-5.3.1
 ~/.pyenv/bin/pyenv global anaconda3-5.3.1
 
 # Keras Tensorflow-gpu install
+echo "Update conda and install some packages"
 conda update -y conda
 conda install -y cudatoolkit==9.0
 conda install -y tensorflow-gpu==1.9.0
