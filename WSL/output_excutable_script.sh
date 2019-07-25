7da7617a2d871fc9c5e25cd9b32f0051e2886a5d
Cramail <y183337@edu.tut.ac.jp>
2019-07-26 00:20:05 +0900

    Bug fixed

--- WSL/output_excutable_script.sh
+++ WSL/output_excutable_script.sh
@@ -2,7 +2,7 @@
 
 if [ -e ~/dotfiles/WSL/setup.sh ]; then
   echo "[*] Output executable setup script to $HOME"
-  cat ~/dotfiles/WSL/setup.sh | egrep -v "/^<<|^CO/" > ~/setup.sh
+  cat ~/dotfiles/WSL/setup.sh | egrep -v "<<CO" | egrep -v "CO" > ~/setup.sh
   echo "[!] Success output"
 fi
 
