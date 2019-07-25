f5e3c277a684893e9c3b2ed0909b579980754741
Cramail <y183337@edu.tut.ac.jp>
2019-07-26 00:06:10 +0900

    Renamed script file

--- WSL/output_excutable_script.sh
+++ WSL/output_excutable_script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 if [ -e ~/dotfiles/WSL/setup.sh ]; then
+  echo "[*] Output executable setup script to $HOME"
   cat ~/dotfiles/WSL/setup.sh | egrep -v "/^<<|^CO/" > ~/setup.sh
+  echo "[!] Success output"
 fi
 
