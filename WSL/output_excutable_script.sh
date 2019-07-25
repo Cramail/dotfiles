845f545048ebaaf70b8edfa43975b480ecb64873
Cramail <y183337@edu.tut.ac.jp>
2019-07-26 00:34:50 +0900

    Fixed path

--- WSL/output_excutable_script.sh
+++ WSL/output_excutable_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ -e ~/dotfiles/WSL/setup.sh ]; then
-  echo "[*] Output executable setup script to $HOME"
-  cat ~/dotfiles/WSL/setup.sh | egrep -v "<<CO" | egrep -v "CO" > ~/setup.sh
+  echo "[*] Output executable setup script to $HOME/setup.sh"
+  cat ~/dotfiles/WSL/setup.sh | egrep -v "<<CO" | egrep -v "CO" > $HOME/setup.sh
   echo "[!] Success output"
 fi
 
