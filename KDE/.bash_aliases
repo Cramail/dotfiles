c10164e47589ea0701968835d854a010a66e043c
Cramail <y183337@edu.tut.ac.jp>
2019-07-26 21:52:54 +0900

    Modified setup script and aliases

--- KDE/.bash_aliases
+++ KDE/.bash_aliases
@@ -1,14 +1,14 @@
 # Aliases
 alias rm='rm -i'
-alias ls='ls --color=auto'
+alias ls='ls --color=auto -X'
 alias la='ls -a'
 alias ll='ls -l'
 alias l='ls -al'
 alias ks='ls'
 alias sl='ls'
-alias ps='ps --sort=start_time'
+alias ps='ps --sort=start_time -f'
 alias vi='vim'
-alias nvidia-smi='watch -n 5 nvidia-smi'
+alias gsmi='watch -n 5 nvidia-smi'
 alias jupyter-notebook="jupyter-notebook --ip=${HOSTNAME}.kde.cs.tut.ac.jp --port=50000"
 alias jupyter-lab="jupyter-lab --ip=${HOSTNAME}.kde.cs.tut.ac.jp --port=50001"
 
--- KDE/setup3.sh
+++ KDE/setup3.sh
@@ -1,4 +1,5 @@
 BRC="~/.bashrc"
+
 # bashrc
 echo "PS1='\${debian_chroot:+(\$debian_chroot)}\[\033[01;36m\]\u@\h\[\033[00m\]:\[\033[01;32m\]\W\[\033[00m\]\$ '" >> $BRC
 echo "##-------- Environment Variables ---------##" >> $BRC
@@ -7,23 +8,23 @@ echo "export EDITOR=vim" >> $BRC
 echo "export LANG=ja_JP.UTF-8" >> $BRC
 
 # vim
-<<CO
+
 if [ -e "~/.vim/colors" ] ; then
-  echo "Create a directory ~/.vim/colors"
+  echo "[*] Create a directory ~/.vim/colors"
   mkdir -p ~/.vim/colors
 fi
 if [ -e "~/.vim/colors/hybrid.vim" ]; then
-  echo "Download hybrid.vim for vim colorscheme"
+  echo "[*] Download hybrid.vim for vim colorscheme"
   git clone https://github.com/w0ng/vim-hybrid.git ~/vim-hybrid
   cp ~/vim-hybrid/colors/hybrid.vim ~/.vim/colors
 fi
-CO
+
 
 # pyenv install
 if [ -e "~/.pyenv" ] ; then
-  echo "Download pyenv for python"
+  echo "[*] Download pyenv for python"
   git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-  echo "Add environment variable for pyenv"
+  echo "[*] Add environment variable for pyenv"
   echo '##-------- pyenv settings --------##' >> $BRC
   echo 'export PYENV_HOME=$HOME/.pyenv' >> $BRC
   echo 'export PATH=$PATH:$PYENV_HOME/bin' >> $BRC
@@ -31,14 +32,15 @@ if [ -e "~/.pyenv" ] ; then
 fi
 
 # anaconda install
-echo "Install Anaconda"
-eval "$(~/.pyenv/bin/pyenv init -)"
-#~/.pyenv/bin/pyenv install anaconda3-5.3.1
-#~/.pyenv/bin/pyenv global anaconda3-5.3.1
+echo "[*] Install anaconda3-5.3.1"
+eval "$(~/.pyenv/bin/pyenv init -)" 
+~/.pyenv/bin/pyenv install anaconda3-5.3.1
+~/.pyenv/bin/pyenv global anaconda3-5.3.1
+~/.pyenv/bin/pyenv rehash
 
-# Keras Tensorflow-gpu install
+# install python packages
 <<CO
-echo "Update conda and install some packages"
+echo "[*] Update conda and install some packages"
 conda update -y conda
 conda install -y cudatoolkit==9.0
 conda install -y tensorflow-gpu==1.9.0
