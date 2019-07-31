7e4c3fed596d98d13682a021bca36dc19594505a
Shinohara Yuichi <shinohara@kde.cs.tut.ac.jp>
2019-07-31 15:54:27 +0900

    Add python setup script for jupyter-lab

---
+++ KDE/setup_python.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "[*] Update conda and pip"
+conda update -y conda
+pip install -U pip
+
+echo "[*] Install packages"
+conda install -y cudatoolkit==9.0
+conda install -y tensorflow-gpu==1.9.0
+conda install -y keras==2.2.4
+pip install progressbar2
+
+echo "[*] Install new node.js"
+curl -OL https://nodejs.org/dist/v10.16.0/node-v10.16.0-linux-x64.tar.xz
+tar -Jxf node-v10.16.0-linux-x64.tar.xz
+mv node-v10.16.0.linux-x64 /home/shinohara/.nodejs
+mv /home/shinoahra/.nodejs/bin/node /home/shinohara/.nodejs/bin/nodejs
+rm -rf node-v10.16.0-linux-x64.tar.xz
+echo -e "\nPATH=$PATH:/home/shinohara/.nodejs/bin" >> /home/shinohara/.bashrc
+
+echo "[*] Enable jupyter-lab's extentions"
+jupyter labextension install @jupyter-widgets/jupyterlab-manage
+jupyter nbextension enable --py --sys-prefix widgetsnbextension
+jupyter labextension install @lckr/jupyterlab_variableinspector
+
+
+
+
