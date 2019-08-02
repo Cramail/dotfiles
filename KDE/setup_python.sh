bc2f59986e993a06b5e0b3166f28acd70708159e
Cramail <y183337@edu.tut.ac.jp>
2019-08-02 16:01:08 +0900

    Not understand

--- KDE/setup_python.sh
+++ KDE/setup_python.sh
@@ -13,15 +13,13 @@ pip install progressbar2
 echo "[*] Install new node.js"
 curl -OL https://nodejs.org/dist/v10.16.0/node-v10.16.0-linux-x64.tar.xz
 tar -Jxf node-v10.16.0-linux-x64.tar.xz
-mv node-v10.16.0.linux-x64 /home/shinohara/.nodejs
+mv node-v10.16.0.linux-x64 $HOME/.nodejs
 rm -rf node-v10.16.0-linux-x64.tar.xz
-echo -e "\nexport PATH=$PATH:/home/shinohara/.nodejs/bin" >> /home/shinohara/.bashrc
+echo -e '\n##--------- node.js settings ---------##' >> $HOME/.bashrc
+echo -e 'export PATH=$PATH:/home/shinohara/.nodejs/bin' >> $HOME/.bashrc
 
 echo "[*] Enable jupyter-lab's extentions"
 jupyter labextension install @jupyter-widgets/jupyterlab-manager
 jupyter nbextension enable --py --sys-prefix widgetsnbextension
 jupyter labextension install @lckr/jupyterlab_variableinspector
-
-
-
 
